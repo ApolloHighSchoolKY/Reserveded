531989cc4a61085565fd4df8513a78c04d219b93
brzle <42354578+brzle@users.noreply.github.com>
2019-02-08 07:57:07 -0600

    Added Scroll Bar
    The scroll bar allows you to go down the page while also renaming the app to Reserved in the code.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
-import 'pages/landing_page.dart';
-//import 'pages/table_page.dart';
 
-void main() {
-  runApp(new MaterialApp(
-    home: new LandingPage(),
-  ));
+void main() => runApp(Reserved());
+
+class Reserved extends StatelessWidget{
+@override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title:"Reserved",
+      home: new Center(
+        child: new Scaffold(
+          appBar: new AppBar(
+            title: new Text("Reserve a table"),
+          ),
+          body:new Center(
+            child: new Text("Center"),
+            ),
+
+        ),
+      ),
+    );
+    }
 }
+
+
+ 
