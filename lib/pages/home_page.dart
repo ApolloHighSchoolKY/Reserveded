f4562b6e7ef81b625c196c7a1a761058171be298
brzle <42354578+brzle@users.noreply.github.com>
2019-02-22 13:07:01 -0600

    Fancy Drawer
    Added more things to the drawer such as account, pages to navigate to, and closing it in the drawer.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './other_page.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -13,21 +14,43 @@ class _HomePageState extends State<HomePage> {
       drawer: new Drawer(
         child: new ListView(
           children: <Widget>[
+
+            new UserAccountsDrawerHeader(
+              accountName: new Text("Brother Montgomery"),
+              accountEmail: new Text("Brother@gmail.com"),
+              currentAccountPicture: new GestureDetector(
+                onTap: () => print("test"),
+                child: new CircleAvatar(
+                  backgroundImage: new NetworkImage("http://apollopltw.weebly.com/uploads/3/7/7/0/37708467/5799917.jpg?140"),
+                ),
+              ),
+              decoration: new BoxDecoration(
+                image: new DecorationImage(
+                  fit: BoxFit.fill,
+                  image: new NetworkImage("https://www.freepik.com/premium-vector/red-black-arrow-overlap-gray-background_2916579.htm")
+
+                )
+              )
+            ),
+
             new ListTile(
               title: new Text("First page"),
               trailing: new Icon(Icons.arrow_upward),
+              onTap: () => Navigator.of(context).push(new MaterialPageRoute(builder:(BuildContext context) => new OtherPage("First Page"))) ,
             ),
 
              new ListTile(
               title: new Text("Second page"),
               trailing: new Icon(Icons.arrow_right),
+              onTap: () => Navigator.of(context).push(new MaterialPageRoute(builder:(BuildContext context) => new OtherPage("Second Page"))) ,
               ),
 
               new Divider(),
-              
+
               new ListTile(
               title: new Text("Close"),
-              trailing: new Icon(Icons.cancel), 
+              trailing: new Icon(Icons.cancel),
+              onTap: () => Navigator.of(context).pop(),
               )
 
           ],
