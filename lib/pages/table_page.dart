79c22721b2e5904f4d800ef62a05841d82edd05a
NotLucas <42354300+NotLucas@users.noreply.github.com>
2019-02-25 12:40:45 -0600

    Firebase Login, maybe
    This should be the working version of firebase login.

--- lib/pages/table_page.dart
+++ lib/pages/table_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 //The page where tables exist
-class TablePage extends StatelessWidget {
+class TablePage extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return new Material(
