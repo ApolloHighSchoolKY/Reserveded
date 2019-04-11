d61c95f43a0f3a622646de593e747884517ba2da
brzle <42354578+brzle@users.noreply.github.com>
2019-04-11 13:26:27 -0500

    Changing Color on App
    Made the color red to be consistent with our board.

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,18 @@ import 'package:flutter/material.dart';
 import 'pages/landing_page.dart';
 
 void main() => runApp(Reserved());
+const PrimaryColor = const Color(0x8C0004);
 
 class Reserved extends StatelessWidget{
 @override
   Widget build(BuildContext context) {
     return new MaterialApp(
       title:"Reserved",
-      color: Colors.red,
       home: new Center(
         child: new Scaffold(
           appBar: new AppBar(
             title: new Text("Please Login"),
+            backgroundColor: PrimaryColor,
           ),
           body:new LandingPage(),
         ),
