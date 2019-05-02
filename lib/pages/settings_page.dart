7276d79ef3a733d0d2274d4e82e10856ad4b66d0
brzle <42354578+brzle@users.noreply.github.com>
2019-05-02 12:51:05 -0500

    Added a settings page
    I made a settings page that will display user profile picture and allow them to upload a picture if they want.

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -4,7 +4,7 @@ import 'package:path_provider/path_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 
-class EmployeePage extends StatelessWidget {
+class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context){
     return new Scaffold(
